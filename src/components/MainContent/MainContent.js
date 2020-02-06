ffa4c33686908421a44b4fd95aa5f529e32f26cf
Arnish Grover <arnishgrover@gmail.com>
2020-02-07 03:27:54 +0530

    trying new things

---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MainContent extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is the Main content</p>
+            </div>
+        )
+    }
+}
+
+export  default MainContent;
