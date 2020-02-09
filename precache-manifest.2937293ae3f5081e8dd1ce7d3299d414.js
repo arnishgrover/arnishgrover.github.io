9527d8ca5ca2d973ecc64efb673edce061b9f563
Arnish Grover <arnishgrover@gmail.com>
2020-02-10 03:02:31 +0530

    Updates

---
+++ precache-manifest.2937293ae3f5081e8dd1ce7d3299d414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8df402a16776c968192cfc261bf7877",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b3aec70a9f07aa1940c",
+    "url": "/static/css/main.4c5a4a2d.chunk.css"
+  },
+  {
+    "revision": "19a2dc635f2e6ecc9d26",
+    "url": "/static/js/2.6bd75a28.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.6bd75a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3aec70a9f07aa1940c",
+    "url": "/static/js/main.3bd3bc74.chunk.js"
+  },
+  {
+    "revision": "d909c8d8aab2c0a4e2e5",
+    "url": "/static/js/runtime-main.1780a330.js"
+  },
+  {
+    "revision": "2d1cc05b204002f9fd957a4355ed9d24",
+    "url": "/static/media/profile.2d1cc05b.jpeg"
+  }
+]);
